8b2566ad3610a02efb5a7e85d089b240fd3df332
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2023-09-04 00:58:31 +0530

    sending post as json

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-import  express  from "express";
+import  express, {json}  from "express";
 //const express =require("express"); declare express
 
 const app =express(); //create express instance 
 const PORT = 5000;// introduce runing port
 
+app.use(json());//req respone send json format 
+
 let posts =[];
+app.get("/posts",(req, res)=>{ //req=server eken ena dewal, res= api ywana dewal
+    res.json(posts);
+});
 
 app.listen(PORT, ()=>{
     console.log(`server is running on port ${PORT}`); // listent to responce of 5000 port
